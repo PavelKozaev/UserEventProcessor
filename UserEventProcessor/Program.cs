94eb08e4fa5cdeb648362e904013a53c4a0290c5
PavelKozaev <pkozaev221299@gmail.com>
2025-07-16 10:22:31 +0300

    feat: create application host

--- UserEventProcessor/Program.cs
+++ UserEventProcessor/Program.cs
@@ -1,4 +1,39 @@
-var builder = Host.CreateApplicationBuilder(args);
+using UserEventProcessor.Data;
+using UserEventProcessor.Services;
 
-var host = builder.Build();
-host.Run();
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        Console.WriteLine("Настройка сервисов...");
+
+        services.AddSingleton<EventObservable>();
+
+
+        var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Переменная окружения POSTGRES_CONNECTION_STRING не установлена.");
+        }
+
+        services.AddNpgsqlDataSource(connectionString);
+
+        services.AddSingleton<IDataStorage, PostgresDataStorage>();
+
+        services.AddSingleton<StatisticsAggregatorObserver>();
+
+        services.AddHostedService<KafkaConsumerService>();
+    })
+    .Build();
+
+Console.WriteLine("Соединение Observable и Observer...");
+var eventObservable = host.Services.GetRequiredService<EventObservable>();
+var statsObserver = host.Services.GetRequiredService<StatisticsAggregatorObserver>();
+
+IDisposable subscription = eventObservable.AsObservable().Subscribe(statsObserver);
+
+Console.WriteLine("Запуск приложения...");
+
+await host.RunAsync();
+
+subscription.Dispose();
+Console.WriteLine("Приложение остановлено.");
