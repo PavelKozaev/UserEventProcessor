62f269061d20bf5ba6525f1ae20e4d2fa499ac71
PavelKozaev <pkozaev221299@gmail.com>
2025-07-16 10:12:26 +0300

    feat: implement an event processing system

---
+++ UserEventProcessor/Services/EventObservable.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using UserEventProcessor.Models;
+
+namespace UserEventProcessor.Services
+{
+    public class EventObservable : IDisposable
+    {
+        private readonly ISubject<UserEvent> _subject = new Subject<UserEvent>();
+
+        public void Publish(UserEvent userEvent)
+        {
+            _subject.OnNext(userEvent);
+        }
+
+        public void Complete()
+        {
+            _subject.OnCompleted();
+        }
+
+        public IObservable<UserEvent> AsObservable()
+        {
+            return _subject.AsObservable();
+        }
+
+        public void Dispose()
+        {
+            (_subject as IDisposable)?.Dispose();
+        }
+    }
+}
